0ee3c4a5f69836206b8300c03424832f5af726c2
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-06-12 14:29:47 +0700

    post text

--- components/posts.js
+++ components/posts.js
@@ -7,7 +7,7 @@ function Posts() {
     <div className='post-container'>
       <Image src={postImage} width={100} height={100} alt='post-image' className='post-image'/>
       <p style={{paddingLeft:20,textTransform:'uppercase'}}>Installation</p>
-      <h4 style={{fontSize:24,fontWeight:500,textTransform:'capitalize',paddingLeft:20}}>what are all the different roof tiles?</h4>
+      <h4 style={{fontSize:24,fontWeight:500,textTransform:'capitalize',paddingLeft:20}}>what are all the different  Ceiling boards?</h4>
       <p style={{paddingLeft:20,}}>November 20 2024   1 Comment</p>
     </div>
   )

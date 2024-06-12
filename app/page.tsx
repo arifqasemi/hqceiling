de46458d39fe33ff4b1fb42542c72552ff424de7
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-06-12 11:50:09 +0700

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Head from "next/head";
+import Head from 'next/head';
+import Image from 'next/image';
+import SectionHero from '../components/section-hero'
+import AboutSection from '../components/about-section'
+import ServiceSection from '../components/service-section'
+import ProjectSection from '../components/project-section'
+import OrderSection from '../components/order-section'
+import VideoSection from '../components/video-section'
+import TestimonialSection from '../components/testimonial-section'
+import TeamSection from '../components/team-section'
+import BlogSection from '../components/blog-section'
+import Space from '../components/space'
+export const metadata = {
+  title: 'Home - HQ Ceiling',
+  description: 'Learn more about us on the About Us page.',
+};
 
 export default function Home() {
-  const title = "About Us - My Next.js App";
-  const description = "Learn more about us on the About Us page.";
-
   return (
     <>
       <Head>
-        <title>{title}</title>
-        <meta name="description" content={description} />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
       </Head>
       <div>
-        <h1>About Us</h1>
-        <p>This is the about page content.</p>
+        <SectionHero/>
+        <AboutSection/>
+        <Space/>
+        <ServiceSection/>
+        <ProjectSection/>
+        <OrderSection/>
+        <VideoSection/>
+        <TestimonialSection/>
+        <Space/>
+        <TeamSection/>
+        <BlogSection/>
       </div>
     </>
   );

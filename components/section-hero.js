606836b5e5d08f1bb73b8be07ea2436e945987aa
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-06-12 08:27:51 +0700

    debug

--- components/section-hero.js
+++ components/section-hero.js
@@ -33,7 +33,7 @@ function SectionHero() {
             <div className='over-lay'>
               <motion.div className='slide-text' initial="hidden" whileInView="visible" variants={slideInVariants} viewport={{ once: true }}>
               <h3 className='slide-heading' >we provide outstanding<br/> construction service</h3>
-              <p style={{color:'white',textAlign:'start',fontSize:16}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>Aenean fermentum odio at diam rutrum,</p>
+              <p style={{color:'white',textAlign:'start',fontSize:16}} className='slide-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>Aenean fermentum odio at diam rutrum,</p>
               <button style={{backgroundColor:'#fe574f',border:'none',padding:15,textTransform:'uppercase',fontWeight:700,color:'white',cursor:'pointer'}}>Learn More</button>
               </motion.div>
             </div>

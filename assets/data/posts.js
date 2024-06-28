b181dcedbdb6f6b6c5e90cfc3514e91fdee31ac8
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-06-28 09:51:29 +0700

    blog-content-update

--- assets/data/posts.js
+++ assets/data/posts.js
@@ -4,7 +4,7 @@ import Posts from "./Model"
 const AllPosts = [
     new Posts('The Importance of Reliable Weatherboard House Builders','With their timeless charm and remarkable durability, weatherboard houses have proven to be enduringly popular among homeowners. They offer a classic aesthetic that blends seamlessly','/images/post-image.jpg'),
     new Posts('That’s a Wrap!','That’s a Wrap! As we fast approach the end of 2023, we just want to thank everyone for their support and commitment over the past','/images/post-image-0.jpg'),
-    new Posts('Our Growing Team','New Estimator We are very excited to have Brent join our team as one of our Estimators! With his experience, he is a great addition. ','/images/post-image-3.jpg')
+    new Posts('Top Trends in Home Design for 2024','Explore the latest trends in home design, from open-concept living spaces to sustainable materials. ','/images/post-image-3.jpg')
     
 
 ]

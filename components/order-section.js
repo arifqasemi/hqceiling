8dd2edf7ea22a3f0dc40f35c1e2c463ee6b5102e
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2024-06-29 13:40:17 +0700

    why choose us

--- components/order-section.js
+++ components/order-section.js
@@ -8,7 +8,7 @@ function OrderSection() {
     <div className='order-section'>
       <p  className='order-sub-heading' style={{fontWeight:500}}>ALWAYS HERE FOR YOU.</p>
         <h2 className='order-heading'>Why Choose Us?</h2>
-        <p style={{textAlign:'center',lineHeight:2,paddingLeft:10,paddingRight:10}}>Throughout the building process, there are numerous decisions, stresses, choices, questions, problems, and queries that arise. From colors, to products, to energy efficiency and bushfire conditions to complex engineering challenges and architectural features, you need a partner in your project, not just a builder. The team at IQ Constructions are dedicated to assisting our clients in sharing our knowledge, providing alternative opinions and giving guidance where necessary. </p>
+        <p style={{textAlign:'center',lineHeight:2,paddingLeft:10,paddingRight:10}}>Throughout the building process, there are numerous decisions, stresses, choices, questions, problems, and queries that arise. From colors, to products, to energy efficiency and bushfire conditions to complex engineering challenges and architectural features, you need a partner in your project, not just a builder. The team at HQ Ceiling are dedicated to assisting our clients in sharing our knowledge, providing alternative opinions and giving guidance where necessary. </p>
       </div>
   )
 }

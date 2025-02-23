a692e9073cae4f342c4a29311744785d33a1492c
arif qasemi <109466958+omarsutlani@users.noreply.github.com>
2025-02-23 16:43:49 +0700

    working on service section

--- components/service-section.js
+++ components/service-section.js
@@ -1,7 +1,5 @@
 'use client'
 import React, { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 import './service-section.css'
 
 import { motion } from 'framer-motion';
@@ -9,7 +7,8 @@ import HomeTab from './tabs/homeTab'
 import RenovationTab from './tabs/renovationTab'
 import CommercialTab from './tabs/commercialTab'
 import Image from 'next/image';
-
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function ServiceSection() {
   const [homeTab,setHomeTab] = useState(true)
   const [renotionTab,setRenotionTab] = useState(false)
@@ -38,17 +37,50 @@ function ServiceSection() {
   return (
     <div className='service-section'>
      <div className='service-section-wrapper'>
-        <h3>Our services  </h3>
+        <h3 style={{color:'#ffb703',textTransform:'uppercase'}}>Our services  </h3>
         <h2 style={{fontSize:56,fontWeight:800}}>Our construction services</h2>
-        <p style={{fontSize:16,marginTop:20}}>We specialize in a wide range of construction services, including residential, commercial, and industrial projects.</p>
+        <p style={{fontSize:16,marginTop:20,color:'#667282'}}>We specialize in a wide range of construction services, including residential, commercial, and industrial projects.</p>
+        <div className='services'>
         <div className='service'>
           <div className='service-content'>
-            <h3 style={{fontSize:24,fontWeight:700,marginBottom:30,color:'white'}}>Building Construction</h3>
+            <h3 style={{fontSize:24,fontWeight:700,marginBottom:20,color:'white'}}>Building Construction</h3>
             <p style={{color:'white'}}>Our post-construction services gives you peace of mind knowing that we are still here for you even after.</p>
-             <div className='service-cta'>
-               <h4>View More</h4>
+             <div className='service-cta' style={{display:'flex',alignItems:'center',cursor:'pointer'}}>
+               <h4 style={{color:'#ffb703',fontWeight:700,marginTop:10,marginRight:10}}>View More</h4>
+               <FontAwesomeIcon icon={faArrowRight} color='#ffb703' style={{marginTop:12}}/>
              </div>
           </div>
+        </div>
+        <div className='service'>
+          <div className='service-content'>
+            <h3 style={{fontSize:24,fontWeight:700,marginBottom:20,color:'white'}}>Building Construction</h3>
+            <p style={{color:'white'}}>Our post-construction services gives you peace of mind knowing that we are still here for you even after.</p>
+             <div className='service-cta' style={{display:'flex',alignItems:'center',cursor:'pointer'}}>
+               <h4 style={{color:'#ffb703',fontWeight:700,marginTop:10,marginRight:10}}>View More</h4>
+               <FontAwesomeIcon icon={faArrowRight} color='#ffb703' style={{marginTop:12}}/>
+             </div>
+          </div>
+        </div>
+        <div className='service'>
+          <div className='service-content'>
+            <h3 style={{fontSize:24,fontWeight:700,marginBottom:20,color:'white'}}>Building Construction</h3>
+            <p style={{color:'white'}}>Our post-construction services gives you peace of mind knowing that we are still here for you even after.</p>
+             <div className='service-cta' style={{display:'flex',alignItems:'center',cursor:'pointer'}}>
+               <h4 style={{color:'#ffb703',fontWeight:700,marginTop:10,marginRight:10}}>View More</h4>
+               <FontAwesomeIcon icon={faArrowRight} color='#ffb703' style={{marginTop:12}}/>
+             </div>
+          </div>
+        </div>
+        <div className='service'>
+          <div className='service-content'>
+            <h3 style={{fontSize:24,fontWeight:700,marginBottom:20,color:'white'}}>Building Construction</h3>
+            <p style={{color:'white'}}>Our post-construction services gives you peace of mind knowing that we are still here for you even after.</p>
+             <div className='service-cta' style={{display:'flex',alignItems:'center',cursor:'pointer'}}>
+               <h4 style={{color:'#ffb703',fontWeight:700,marginTop:10,marginRight:10}}>View More</h4>
+               <FontAwesomeIcon icon={faArrowRight} color='#ffb703' style={{marginTop:12}}/>
+             </div>
+          </div>
+        </div>
         </div>
      </div>
 
